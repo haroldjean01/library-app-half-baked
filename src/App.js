d2b47aeeac94472da1979ffd37149587f485fba3
Harold Jean <haroldjean38@yahoo.com>
2023-01-23 18:51:04 -0500

    deconstructed id in BookDetail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import BookList from './components/book/BookList';
+import BookList from './components/book/BookList.js';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Book from './components/book/Book.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        {/* <h1>Library Catalog</h1> */}
+        <Route exact path="/books" component={Book} />
+        <Route exact path="/booklist" component={BookList} />
+        <BookList />
+      </Switch>
     </main>
   );
 }
