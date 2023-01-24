1eb14092f82797f5b29f1f717cc8210370b8ba36
Harold Jean <haroldjean38@yahoo.com>
2023-01-23 19:16:18 -0500

    added route to BookDetail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BookList from './components/book/BookList.js';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Book from './components/book/Book.js';
+import BookDetail from './components/book/BookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
@@ -10,7 +11,8 @@ function App() {
       <Switch>
         {/* <h1>Library Catalog</h1> */}
         <Route exact path="/books" component={Book} />
-        <Route exact path="/booklist" component={BookList} />
+        <Route exact path="/book-list" component={BookList} />
+        <Route exact path="/books/:id" component={BookDetail} />
         <BookList />
       </Switch>
     </main>
